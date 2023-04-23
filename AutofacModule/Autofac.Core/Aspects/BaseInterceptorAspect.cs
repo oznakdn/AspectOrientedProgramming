dbfc78803250b7b8d564c9ede19d615e2a771d56
Ozan Akaydin <ozanakaydin@gmail.com>
2023-04-23 14:05:20 +0300

    Autofac module

---
+++ AutofacModule/Autofac.Core/Aspects/BaseInterceptorAspect.cs
@@ -0,0 +1,23 @@
+﻿using Castle.DynamicProxy;
+
+namespace Autofac.Core.Aspects
+{
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class,AllowMultiple =true,Inherited =true)]
+    public class BaseInterceptorAspect : Attribute, IInterceptor
+    {
+        public virtual void Intercept(IInvocation invocation)
+        {
+            Console.WriteLine($"{invocation.Method} method running");
+             invocation.Proceed();
+        }
+
+        public virtual void Before() => Console.WriteLine("Before Method");
+        public virtual void After() => Console.WriteLine("After Method");
+        public virtual void onSuccess(string? successMessage) { }
+        public virtual void onFailure(string? failureMessage) { }
+        public virtual void onException (Exception ex) { }
+
+
+    }
+}
---
+++ AutofacModule/Autofac.Core/Aspects/LoggingInterceptorAspect.cs
@@ -0,0 +1,40 @@
+﻿using Castle.DynamicProxy;
+
+namespace Autofac.Core.Aspects
+{
+
+    public class LoggingInterceptorAspect : BaseInterceptorAspect
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            this.Before();
+            var parameters = invocation.Method.GetParameters();
+            foreach (var parameter in parameters)
+            {
+                if (parameter.Member is null)
+                    onFailure("Parameter can not be null!");
+                else
+                {
+                    try
+                    {
+                        base.Intercept(invocation);
+                        this.onSuccess("Employee is added.");
+                    }
+                    catch (Exception ex)
+                    {
+                        this.onException(ex);
+                    }
+                    finally
+                    {
+                        this.After();
+                    }
+                }
+            }
+        }
+
+        public override void onSuccess(string? successMessage) => Console.WriteLine(successMessage);
+        public override void onFailure(string? failureMessage) => Console.WriteLine(failureMessage);
+
+
+    }
+}
