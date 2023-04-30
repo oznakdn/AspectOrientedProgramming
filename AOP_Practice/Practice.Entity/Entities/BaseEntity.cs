a0a766731bb46af78cf842f275703de2c06807a9
Ozan Akaydin <ozanakaydin@gmail.com>
2023-04-30 14:37:38 +0300

    some changes

--- AOP_Practice/Practice.Entity/Entities/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Practice.Entity.Entities
-{
-    public abstract class BaseEntity
-    {
-        public int Id { get; set; }
-        public bool IsActice { get; set; } = true;
-    }
-}
